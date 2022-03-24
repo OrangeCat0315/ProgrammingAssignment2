ad67f7fbc604c8e356b8696d56e6cdd962070aaf
Euclid <militan0315@gmail.com>
2022-03-24 16:31:43 +0900

    The function caching the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R script is about a R function caching the inverse of a matrix by 'Orange Cat'
 
-## Write a short comment describing this function
+
+## 'makeCacheMatrix' is a function which makes a input matrix 'Special Matrix' that fit into caching function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  set_invmtx <- function(invmtx) i <<- invmtx
+  get_invmtx <- function() i
+  list(set = set, get = get,
+       set_invmtx = set_invmtx,
+       get_invmtx = get_invmtx)
+  
 }
 
 
-## Write a short comment describing this function
+## 'cacheSolve' is a cache tracking function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$get_invmtx()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$set_invmtx(i)
+  i
 }
